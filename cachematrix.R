e09ccfeb0e54de2b2e504ea78f4ff77db84ac50c
AKiskin <estel4ever@gmail.com>
2015-06-16 17:44:27 +0300

    Task complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create a quasi-matrix object from real matrix
+## get original matrix by get()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calc inverse matrix using makeCacheMatrix() quasi-matrix object
+## if called repeatedly, does not actually calculate, but gets inversed value calculated on first call
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+ 
 }
-
-
-##test commit
